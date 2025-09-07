17a2446b0ad869c7048978c1c0d3df20814a370d
JUNAID <moinuddinm942@gmail.com>
2025-09-07 19:32:54 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="HEALTHBOT",
     version="0.1",
     author="MOINUDDIN",
-    author_email="moinuddinm942@gmail.com"
+    author_email="moinuddinm942@gmail.com",
     packages=find_packages(),
-    install requires=[]
+    installrequires=[]
 )
