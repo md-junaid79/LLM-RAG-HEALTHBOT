e79cd86403ad509a522c57d57e49af859be15f73
JUNAID <moinuddinm942@gmail.com>
2025-09-07 19:30:25 +0530

    requirements/libraries added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="HEALTHBOT",
+    version="0.1",
+    author="MOINUDDIN",
+    author_email="moinuddinm942@gmail.com"
+    packages=find_packages(),
+    install requires=[]
+)
